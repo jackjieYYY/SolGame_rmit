d6c644b4246c6cfb113468fb033fe210e4033953
Nguyen Hoang Hai (S3822918) <s3822918@rmit.edu.vn>
2022-03-31 08:39:50 +0700

    Minor changes in Seek and Arrive

--- Assets/Script/Player/MoveWithMouse.cs
+++ Assets/Script/Player/MoveWithMouse.cs
@@ -17,6 +17,7 @@ public class MoveWithMouse : MonoBehaviour
 
     [SerializeField] private float maxForce =  15f, maxSpeed = 15f, slowingRadius = 0.5f, drag = 4f;
     [SerializeField] private float pathRadius = 0.00001f, futureAhead = 0.1f;
+    Vector3 steeringForce = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -46,6 +47,10 @@ public class MoveWithMouse : MonoBehaviour
             {
                 Debug.DrawLine(path[i], path[i+1], Color.cyan);
             }
+            for(int i = 0; i < path2.Count - 1; i++)
+            {
+                Debug.DrawLine(path2[i], path2[i+1], Color.white);
+            }
         }
         catch
         {
@@ -53,6 +58,12 @@ public class MoveWithMouse : MonoBehaviour
         }
         // FollowPath();// this is  original method.
         FollowPath(path);// this is new one with AddForce. But need to Fix.
+
+        // Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // target.y = 0;
+        // steeringForce +=  obstacleAvoidance() + Seek(target, m_Rigidbody.position, m_Rigidbody.velocity, maxSpeed, maxForce);
+        // m_Rigidbody.AddForce(steeringForce);
+        // m_Rigidbody.velocity = Vector3.ClampMagnitude(m_Rigidbody.velocity, maxForce);
     }
 
 
@@ -60,9 +71,27 @@ public class MoveWithMouse : MonoBehaviour
     void TryGetPath(Vector3 end)
     {
         PathManager.Request(transform.position, end, true, onPathFound);
+        PathManager.Request(transform.position, end, false, onPathFound2);
     }
 
+    Vector3 obstacleAvoidance()
+    {
+        Vector3 steering = Vector3.zero;
+        RaycastHit hit;
+        Vector3 target = m_Rigidbody.velocity;
+        target.y = 0;
 
+        Debug.DrawRay(m_Rigidbody.position, target * 10f,Color.green);
+        bool rayHit = Physics.Raycast(m_Rigidbody.position, target, out hit, 1.5f, LayerMask.GetMask("unwalkable"));
+        if (rayHit)
+        {
+            print(hit.distance);
+            Vector3 diff = m_Rigidbody.position - hit.collider.gameObject.transform.position;
+            steering += diff;
+        }
+        steering = steering.normalized * 5f;
+        return steering;
+    }
     void onPathFound(List<Vector3> _path, bool pathSuccessful)
     {
         if (pathSuccessful)
@@ -81,13 +110,13 @@ public class MoveWithMouse : MonoBehaviour
     {
         return Vector3.Dot((end - start).normalized, (point - end).normalized) < 0f && Vector3.Dot((start - end).normalized, (point - start).normalized) < 0f;
     }
-    public static Vector3 Seek(Vector3 target, Vector3 position, Vector3 currentVelocity, float maxSpeed)
+    public static Vector3 Seek(Vector3 target, Vector3 position, Vector3 currentVelocity, float maxSpeed, float maxForce)
     {
         Vector3 desiredVelocity = (target - position).normalized * maxSpeed;
-        Vector3 steering = desiredVelocity - currentVelocity;
+        Vector3 steering = Vector3.ClampMagnitude(desiredVelocity - currentVelocity, maxForce);
         return steering;
     }
-    public static Vector3 Arrive(Vector3 target, Vector3 position, Vector3 currentVelocity, float maxSpeed, float slowingRadius)
+    public static Vector3 Arrive(Vector3 target, Vector3 position, Vector3 currentVelocity, float maxSpeed, float maxForce, float slowingRadius)
     {
         Vector3 desiredVelocity = (target - position);
         float distance = desiredVelocity.magnitude;
@@ -99,7 +128,7 @@ public class MoveWithMouse : MonoBehaviour
         {
             desiredVelocity = desiredVelocity.normalized * maxSpeed;
         }
-        Vector3 steering = desiredVelocity - currentVelocity;
+        Vector3 steering = Vector3.ClampMagnitude(desiredVelocity - currentVelocity, maxForce);
         return steering;
     }
 
@@ -148,14 +177,12 @@ public class MoveWithMouse : MonoBehaviour
 
             if(temp > pathRadius)
             {
-                m_Rigidbody.AddForce(Seek(target, m_Rigidbody.position, m_Rigidbody.velocity, maxSpeed));
-                m_Rigidbody.velocity = Vector3.ClampMagnitude(m_Rigidbody.velocity, maxForce); 
+                m_Rigidbody.AddForce(Seek(target, m_Rigidbody.position, m_Rigidbody.velocity, maxSpeed, maxForce));
             }
             if(Vector3.Distance(pathList[pathList.Count-1], m_Rigidbody.position) <= 1f)
             {
                 m_Rigidbody.velocity = Vector3.zero;
-                m_Rigidbody.AddForce(Arrive(pathList[pathList.Count-1], m_Rigidbody.position, m_Rigidbody.velocity, maxSpeed, slowingRadius));
-                m_Rigidbody.velocity = Vector3.ClampMagnitude(m_Rigidbody.velocity, maxForce); 
+                m_Rigidbody.AddForce(Arrive(pathList[pathList.Count-1], m_Rigidbody.position, m_Rigidbody.velocity, maxSpeed, maxForce, slowingRadius));
             }
             
         }
