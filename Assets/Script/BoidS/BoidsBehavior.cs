96f366ee56e29ea8b0478b6a313d2426471f1487
jackjieYYY <53422254+jackjieYYY@users.noreply.github.com>
2022-04-12 08:43:26 +1000

    Demo Version

--- Assets/Script/BoidS/BoidsBehavior.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BoidsBehavior : ScriptableObject
-{
-    public abstract Vector3 CalMove(BoidManager manager, List<Transform> context, BoidSpawnerTest BoidSpawner);
-
-}
